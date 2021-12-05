3bdd04c90b346b375089e39fd2b67602c659830c
Mr Tomato <58458857+mrTomatolegit@users.noreply.github.com>
2021-12-05 09:13:34 +0300

    arrow funcs and formatting

--- src/client/Client.js
+++ src/client/Client.js
@@ -16,18 +16,18 @@ class Client extends BaseClient {
  constructor(bot, args) {
   super();
   //Check Whether the client is real or not.
-  if (typeof(bot?.login || bot?.connect) != "function") {
+  if (typeof (bot?.login || bot?.connect) != "function") {
    console.log(`${API_ERROR_PREFIX} Instance of Discord.js or Eris Client not given in arguments.`);
-  if(typeof(args)=="object"){
+   if (typeof (args) == "object") {
     this.args = args;
-  }
+   }
    process.exit(1);
   }
-  bot.on('guildCreate', function(guild) {
+  bot.on('guildCreate', (guild) => {
    //discord.js or eris.
    this.bot.postServers(bot.guilds.cache.size || bot.guilds.size);
   });
-  bot.on('guildDelete', function(guild) {
+  bot.on('guildDelete', (guild) => {
    //discord.js or eris.
    this.bot.postServers(bot.guilds.cache.size || bot.guilds.size);
   });
@@ -48,12 +48,12 @@ class Client extends BaseClient {
   this.client = bot;
 
   this.api = api;
-  
+
   /**
    * @type {Array}
    */
-  this.votes = (this.args.cacheVotes==true)?[]:null;
-  
+  this.votes = (this.args.cacheVotes == true) ? [] : null;
+
   /**
     * @type {Number}
     */
@@ -66,24 +66,23 @@ class Client extends BaseClient {
   * @returns {Promise<ClientBot>}
   */
  login(code) {
-  var that = this;
-  return new Promise(function(resolve, reject){
-   api.bots.fetchBotFromCode(code).then(function(data){
-     that.bot = new ClientBot(that, data);
-     that.currentVotes = that.bot.votes;
-     that.client.on('ready', function() {
-      if (that.bot.id != that.client.user.id) {
-       console.log(`${API_ERROR_PREFIX} Bot Data doesn't match Client data.\nReporting this to RDL support team and the owners of ${that.client.user.username} bot immediately!`);
-       fetch(`${PROTOCOL}://${BASE_HOST}/${API_PATH}/bots/report?leaked=${code}`).then(function() {
-        process.exit(1);
-       });
-      }
-     });
-     that.code = code;
-     that.emit(Events.CLIENT_READY);
-     resolve(that.bot);
-    })
-    .catch(function(e){ reject(e.stack)});
+  return new Promise((resolve, reject) => {
+   api.bots.fetchBotFromCode(code).then(data => {
+    this.bot = new ClientBot(this, data);
+    this.currentVotes = this.bot.votes;
+    this.client.on('ready', () => {
+     if (this.bot.id != this.client.user.id) {
+      console.log(`${API_ERROR_PREFIX} Bot Data doesn't match Client data.\nReporting this to RDL support team and the owners of ${this.client.user.username} bot immediately!`);
+      fetch(`${PROTOCOL}://${BASE_HOST}/${API_PATH}/bots/report?leaked=${code}`).then(() => {
+       process.exit(1);
+      });
+     }
+    });
+    this.code = code;
+    this.emit(Events.CLIENT_READY);
+    resolve(this.bot);
+   })
+    .catch(e => reject(e.stack));
   });
  }
 
@@ -95,28 +94,29 @@ class Client extends BaseClient {
  webhooks() {
   try {
    var express = require('express');
-  } catch {}
+  } catch { }
 
   if (!express) return;
   const router = express.Router();
 
   router.use(express.json('*/*'));
 
-  router.post('/', function(req, res) {
+  router.post('/', (req, res) => {
    if (!req.query.code) return res.status(401).json({ err: 'no_code' });
-   else{
-   if (req.query.code !== this.code) return res.status(401).json({ err: 'invalid_code' });
-   else{
-   this.emit('vote', req.body);
-   if(this.args.cacheVotes){
-   this.votes.push({at: new Date(), user: req.body.user, votes: req.body.votes, coins: req.body.coins});
-   if(typeof this.args.clearVotesAt == "number") setTimeout(function(){
-     this.votes.shift();
-   },this.args.clearVotesAt);
+   else {
+    if (req.query.code !== this.code) return res.status(401).json({ err: 'invalid_code' });
+    else {
+     this.emit('vote', req.body);
+     if (this.args.cacheVotes) {
+      this.votes.push({ at: new Date(), user: req.body.user, votes: req.body.votes, coins: req.body.coins });
+      if (typeof this.args.clearVotesAt == "number") setTimeout(() => {
+       this.votes.shift();
+      }, this.args.clearVotesAt);
+     }
+     this.currentVotes = req.body.currentVotes;
+     res.json({ ok: "true" });
+    }
    }
-   this.currentVotes = req.body.currentVotes;
-   res.json({ ok: "true" });
-   }}
   });
 
   return router;
